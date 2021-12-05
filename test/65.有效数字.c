35e9084724cd8a82b13b00bf94098628b1172462
dishenzongzhiju <dishenzongzhijun@gmail.com>
2021-12-05 23:59:18 +0800

    test

---
+++ test/65.有效数字.c
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=65 lang=c
+ *
+ * [65] 有效数字
+ */
+
+// @lc code=start
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+bool isNumber(char * s){
+
+}
+// @lc code=end
+
