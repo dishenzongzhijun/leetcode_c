66b883149223967b652b77d76b0d56a03a924d95
dishenzongzhiju <dishenzongzhijun@gmail.com>
2022-04-24 01:23:17 +0800

    [feat]:add 69.x-的平方根.c [body]: [scope]: global

--- 65.有效数字.c
+++ 65.有效数字.c
@@ -2,7 +2,7 @@
  * @Author       : liuben
  * @Date         : 2021-12-06 00:10:31
  * @LastEditors  : liuben
- * @LastEditTime : 2022-04-23 22:24:33
+ * @LastEditTime : 2022-04-24 01:05:27
  * @Description  :
  * @FilePath     : /leetcode_c/65.有效数字.c
  */
@@ -17,20 +17,9 @@
 
 // @lc code=start
 
-bool isNumber(int num)
+bool isNumber(char *s)
 {
     return true;
 }
 
 // @lc code=end
-
-int main()
-{
-    int num = 9;
-    bool res = false;
-    printf("ok:%d\n", num);
-    num += 3;
-    printf("ok:%d\n", num);
-    res = isNumber(num);
-    return 0;
-}
--- 69.x-的平方根.c
+++ 69.x-的平方根.c
@@ -2,7 +2,7 @@
  * @Author       : liuben
  * @Date         : 2022-04-22 20:11:52
  * @LastEditors  : liuben
- * @LastEditTime : 2022-04-22 20:50:15
+ * @LastEditTime : 2022-04-24 01:21:31
  * @Description  : 
  * @FilePath     : /leetcode_c/69.x-的平方根.c
  */
@@ -19,6 +19,32 @@
 
 int mySqrt(int x)
 {
-    int num; 
+    // O(n)
+    int res = 0;
+    for (int i = 0; i < x; i++) {
+        if (i * i > x) {
+            res = i - 1;
+        }
+    }
+
+    return res;
+
+    // O(log n)
+    int left = 0;
+    int right = x;
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        int tmp = mid * mid;
+        if (tmp == x) {
+            right = mid;
+        } else if (tmp < x) {
+            left = mid + 1;
+        } else if (tmp > x) {
+            right = mid;
+        }
+    }
+    return left;
 }
 // @lc code=end
