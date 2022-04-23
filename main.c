c371e807a2cd841f0ecb5a48f3a8a9dbd68e78db
dishenzongzhiju <dishenzongzhijun@gmail.com>
2022-04-23 23:59:56 +0800

    [feat]: 测试amend [body]:amend [scope]: global

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * @Author       : liuben
  * @Date         : 2022-04-23 22:35:28
  * @LastEditors  : liuben
- * @LastEditTime : 2022-04-23 23:55:13
+ * @LastEditTime : 2022-04-23 23:59:22
  * @Description  : general main function
  * @FilePath     : /leetcode_c/main.c
  */
@@ -27,6 +27,8 @@ int main()
 
     free(nums_a);
     free(nums_b);
+
+    printf("amend");
 
     return 0;
 }
